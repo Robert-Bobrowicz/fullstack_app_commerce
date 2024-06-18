f94dab520f88aea467761acad3b50cc027752961
unknown <r.bobrowicz@live.com>
2024-06-18 11:31:22 +0200

    fullstack app training

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
+    res.send({ hi: 'there at the other part of the World' })
 });
 
 app.listen(PORT);
